5363cd745b78b72a77bc51688116bf8106a4b3c5
Motiff <lqcyt20010122@gmail.com>
2020-04-08 21:55:10 +0800

    project transfer to another pc

--- tttst.js
+++ tttst.js
@@ -1,26 +1,29 @@
-let table;
-let map;
+let myTable;
+let myMap;
 
 function preload(){
-     table=loadTable('data/xinhua.csv','csv','header');
+     myTable=loadTable('data/xinhua.csv','csv','header');
 }
 
 function setup() { 
     createCanvas(480, 130);
     textFont("Arial"); 
     fill(123, 0 , 0);
-    for(let i=0;i<table.getRowCount();i++){
-        let a=table[i].getString("word");
-        let b=table[i].getString('part');
-        map.push(a,b);
-    }
-
+    myMap=myTable.getArray();
 }
 function draw() {
     background(255);  
     textSize(42);  
     text("Hello", 25, 60);
     textSize(26);  
-    text("World", 27, 90)
+    //text("World", 27, 90);
+    let key= "信";
+    for(var i=0;i<myMap.length;i++){
+        if(key==myMap[i][0]){
+            text(myMap[i][1],100,100);
+        }
+        print(map[i][1]);
 
+    }
+    print(key);
 }
