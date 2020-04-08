12a8d43395ad82b809e7b1bf64d285258d24d372
Motifff <lqcyt20010122@gmail.com>
2020-04-08 22:46:59 +0800

    the csv working method in p5.js

--- tttst.js
+++ tttst.js
@@ -6,24 +6,29 @@ function preload(){
 }
 
 function setup() { 
-    createCanvas(480, 130);
-    textFont("Arial"); 
+    createCanvas(480, 480);
     fill(123, 0 , 0);
+    textFont('Arial');
+    myMap=new Array();
     myMap=myTable.getArray();
 }
 function draw() {
-    background(255);  
+    background(200);
     textSize(42);  
     text("Hello", 25, 60);
     textSize(26);  
     //text("World", 27, 90);
     let key= "信";
+    print(myMap.length);
     for(var i=0;i<myMap.length;i++){
-        if(key==myMap[i][0]){
-            text(myMap[i][1],100,100);
+        if(key==myMap[i][0]) {
+            text(myMap[i][1], 100, 300);
+            print('ok');
+            print(i);
         }
-        print(map[i][1]);
 
     }
-    print(key);
+    text("中文",300,100);
+    text(myMap.length,100,100);
+    text(myMap[100][0].toString(),200,100);
 }
