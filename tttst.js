2a25e99768c4398eb491b53990ea1fe87fcc1fdc
Motifff <lqcyt20010122@gmail.com>
2020-04-10 20:47:25 +0800

    the csv working method in p5.js

--- tttst.js
+++ tttst.js
@@ -1,34 +1,75 @@
 let myTable;
 let myMap;
+let myGui;
+let guiSystem;
 
 function preload(){
      myTable=loadTable('data/xinhua.csv','csv','header');
 }
 
-function setup() { 
-    createCanvas(480, 480);
+function mousePressed() {
+    for (let i = 0; i < 4; i++) {
+        if (mouseX < myGui[i].pos[0] + 50 || mouseX > myGui[i].pos[0] - 50) {
+            myGui[i].mode = !myGui[i].mode;
+        }
+    }
+}
+
+function mouseReleased(){
+    for(let i=0;i<4;i++){
+        myGui.tmper[i].mode=true;
+    }
+}
+
+function theGui(){
+    this.data=[];
+    this.len=400
+    this.born=function(){
+        for(let i=0;i<4;i++){
+            myGui[i]=new gui_s(width/2-this.len/2+i*this.len/4,400);
+        }
+    }
+    this.show=function(){
+        for(let i=0;i<4;i++){
+            myGui[i].show();
+        }
+    }
+}
+
+function gui_s(_x, _y){
+    this.pos=[_x,_y];
+    this.mode=true;
+    this.show=function(){
+        switch(this.mode){
+            case true:
+                fill(200);
+            case false:
+                fill(100);
+        }
+        rect(this.pos[0],this.pos[1],100,100);
+    }
+}
+
+function search(a){
+    for(let i=0;i<myMap.length;i++){
+        if (a==myMap[i][0]){
+            return(myMap[i][1]);
+        }
+    }
+    return("NA");
+}
+
+function setup() {
+    createCanvas(windowWidth,windowHeight);
     fill(123, 0 , 0);
     textFont('Arial');
     myMap=new Array();
     myMap=myTable.getArray();
+    theGui.
 }
+
 function draw() {
     background(200);
-    textSize(42);  
-    text("Hello", 25, 60);
-    textSize(26);  
-    //text("World", 27, 90);
-    let key= "信";
-    print(myMap.length);
-    for(var i=0;i<myMap.length;i++){
-        if(key==myMap[i][0]) {
-            text(myMap[i][1], 100, 300);
-            print('ok');
-            print(i);
-        }
+    ;
 
-    }
-    text("中文",300,100);
-    text(myMap.length,100,100);
-    text(myMap[100][0].toString(),200,100);
 }
