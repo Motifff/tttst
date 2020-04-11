a7df834843ab2e26b0f696fff428a8c623613363
Motiff <lqcyt20010122@gmail.com>
2020-04-11 16:12:24 +0800

    a little function program

--- tttst.js
+++ tttst.js
@@ -1,18 +1,19 @@
 let myTable;
 let myMap;
 let guiSystem;
+let wordMap;
 
 function preload(){
      myTable=loadTable('data/xinhua.csv','csv','header');
 }
-
+//gui System below
 function theGui(){
     this.data=[];
     this.len=400;
     this.tmper=[];
     this.born=function(){
         for(let i=0;i<4;i++){
-            this.tmper[i]=new gui_s(width/2-(i-2)*this.len/4,400);
+            this.tmper[i]=new gui_s(width/2-200+i*100,600);
         }
     }
     this.show=function(){
@@ -27,7 +28,7 @@ function gui_s(_x, _y){
     this.mode=true;
     this.show=function(){
         if (this.mode==true) {
-            fill(0);
+            fill(168,216,185);
         } else{
             fill(255);
         }
@@ -36,45 +37,122 @@ function gui_s(_x, _y){
     }
 }
 
-function search(a){
+//character match and generate
+function words() {
+    this.len=8;
+    this.maps=[];
+    this.times=0;
+    this.gen=function(){
+        for(let i=0;i<2;i++){
+            this.maps[i]=new Array();
+            for(let j=0;j<4;j++) {
+                let a = int(random(myMap.length));
+                while (this.maps.indexOf(a) != -1) {
+                    a = int(random(myMap.length));
+                }
+                this.maps[i][j]=a;
+            }
+        }
+    }
+    this.work=function(_b){
+        if(this.maps[1][_b]=="NA") {
+            let a = int(random(myMap.length));
+            while (this.maps.indexOf(a) != -1) {
+                a = int(random(myMap.length));
+            }
+            this.maps[0][_b] = a;
+            let b = int(random(myMap.length));
+            while (this.maps.indexOf(b) != -1) {
+                b = int(random(myMap.length));
+            }
+            this.maps[1][_b] = b;
+        }else{
+            this.maps[0][_b]=this.maps[1][_b];
+            this.maps[1][_b]="NA";
+        }
+        this.times+=1;
+    }
+
+    this.show=function(){
+        for(let j=0;j<4;j++){
+            textSize(50);
+            fill(168,216,185);
+            text(myMap[this.maps[0][j]][1],guiSystem.tmper[j].pos[0],guiSystem.tmper[j].pos[1])
+        }
+    }
+}
+
+
+function search(_a){
     for(let i=0;i<myMap.length;i++){
-        if (a==myMap[i][0]){
+        if (_a==myMap[i][0]){
             return(myMap[i][1]);
         }
     }
     return("NA");
 }
 
-
-function setup() {
-    createCanvas(windowWidth,windowHeight);
-    fill(123, 0 , 0);
-    textFont('Arial');
-    rectMode(CENTER);
-    ellipseMode(CENTER);
-    myMap=new Array();
-    myMap=myTable.getArray();
-    guiSystem=new theGui();
-    guiSystem.born();
-
-}
-
-function draw() {
-    background(200);
-    stroke(255);
+//mouse Actions
+/*
+function mouseAction(){
     if (mouseIsPressed){
         for(let i=0;i<4;i++){
             if(mouseX < guiSystem.tmper[i].pos[0] + 50 && mouseX > guiSystem.tmper[i].pos[0] - 50 && abs(mouseY-400)<100){
                 guiSystem.tmper[i].mode=false;
-                ellipse(guiSystem.tmper[i].pos[0],100,100,100);
+                if(wordMap.times==0) {
+                    wordMap.work(i);
+                }
             }
         }
     } else{
         for(let i=0;i<4;i++){
             guiSystem.tmper[i].mode=true;
         }
+        wordMap.times=0;
+        return("no");
     }
-    print(guiSystem.tmper[1]);
-    guiSystem.show();
+}
+*/
 
+function mousePressed(){
+    for(let i=0;i<4;i++){
+        if(mouseX < guiSystem.tmper[i].pos[0] + 50 && mouseX > guiSystem.tmper[i].pos[0] - 50 && abs(mouseY-600)<100){
+            guiSystem.tmper[i].mode=false;
+            if(wordMap.times==0) {
+                wordMap.work(i);
+            }
+        }
+    }
+}
+
+function mouseReset(){
+    for(let i=0;i<4;i++){
+        guiSystem.tmper[i].mode=true;
+    }
+    wordMap.times=0;
+    return("no");
+}
+
+//p5 drawing parts
+function setup() {
+    createCanvas(1080,720);
+    fill(123, 0 , 0);
+    textFont('Arial');
+    rectMode(CENTER);
+    myMap=new Array();
+    myMap=myTable.getArray();
+    guiSystem=new theGui();
+    guiSystem.born();
+    wordMap=new words();
+    wordMap.gen();
+
+}
+
+function draw() {
+    background(237,120,74);
+    stroke(255);
+    //mouseAction();
+    guiSystem.show();
+    wordMap.show();
+    mouseReset();
 }
